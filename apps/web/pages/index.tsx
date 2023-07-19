01bb1f6dd210860c4c91896fed1c293192871412
AgentosUA <oleh.stepaniuk@chatbots.studio>
2023-07-19 23:58:08 +0300

    rename web app

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -13,7 +13,7 @@ export function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome petfinder-ui 👋
+              Welcome web 👋
             </h1>
           </div>
 
