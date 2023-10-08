4fc3ee32b244e11c2e0aee72c7f37b301e180818
AgentosUA <oleh.stepaniuk@chatbots.studio>
2023-10-09 00:24:57 +0300

    refact: nextjs setup

---
+++ apps/client/entities/user/model.ts
@@ -0,0 +1,36 @@
+import { makeAutoObservable } from 'mobx';
+import { cookies } from 'next/dist/client/components/headers';
+import { requestAsyncStorage } from 'next/dist/client/components/request-async-storage';
+
+class UserModel {
+  public isAuthorized = false;
+
+  constructor() {
+    makeAutoObservable(this);
+
+    const isAuthorized = true;
+
+    if (isAuthorized) {
+      this.setAuthorized(isAuthorized);
+    }
+  }
+
+  async getInfo() {
+    const response = await fetch('/api/user/info');
+    const data = await response.json();
+
+    return data;
+  }
+
+  setAuthorized = (isAuthorized: boolean) => {
+    this.isAuthorized = isAuthorized;
+  };
+
+  logout = () => {
+    this.setAuthorized(false);
+  };
+}
+
+const User = new UserModel();
+
+export { User, UserModel };
--- apps/client/widgets/layout/main/header/ui.tsx
+++ apps/client/widgets/layout/main/header/ui.tsx
@@ -1,11 +1,20 @@
-import styles from './ui.module.scss';
+'use client';
 
 import Logo from '@ui/shared/ui/assets/images/logo.png';
 
 import Image from 'next/image';
+
 import Link from 'next/link';
 
-const Header = () => (
+import { observer } from 'mobx-react-lite';
+
+import { User }  from '@ui/entities/user/model';
+
+import styles from './ui.module.scss';
+
+const Header = observer(() => {
+
+  return (
   <header className={styles.wrapper}>
     <Image className={styles.logo} src={Logo} alt='Petfinder logo' />
     <menu className={styles.menu}>
@@ -14,11 +23,18 @@ const Header = () => (
       <Link className={styles.link} href='/'>Приютити</Link>
       <Link className={styles.link} href='/'>Про проект</Link>
     </menu>
-      <menu className={styles.menu}>
-      <Link className={styles.link} href='/lost'>Увійти</Link>
-      <Link className={styles.link} href='/founded'>Реєстрація</Link>
+    <menu className={styles.menu}>
+    {!User.isAuthorized && <>
+      <Link className={styles.link} href='/auth/login'>Увійти</Link>
+      <Link className={styles.link} href='/auth/signup'>Реєстрація</Link>
+    </>}
+
+    {User.isAuthorized && <>
+      <Link className={styles.link} href='/profile'>Профіль</Link>
+      <button className={styles.link} onClick={User.logout}>Вийти</button>
+    </>}
     </menu>
   </header>
-);
+)});
 
 export { Header };
