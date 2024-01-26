798cc136ebdd8847facd0d51f19e7c8f100af85e
AgentosUA <oleh.stepaniuk@chatbots.studio>
2024-01-27 01:09:15 +0200

    feat: db connect

--- apps/gateway/src/app/app.module.ts
+++ apps/gateway/src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 
+import { Logger } from '@petfinder-project/logger';
+import { SequelizeModule } from '@nestjs/sequelize';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Logger } from '@petfinder-project/logger';
 
 @Module({
-  imports: [Logger],
+  imports: [Logger, SequelizeModule.forRoot({
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_USER,
+    database: process.env.DB_NAME,
+    models: [],
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
